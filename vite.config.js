914b4a6862c5c61771b836ff4cdca5919c23e9af
dageboyi <75166480+dageboyi@users.noreply.github.com>
2023-11-08 21:13:44 +1030

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {// 本地开发环境通过代理实现跨域，生产环境使用 nginx 转发
       '^/api': {
-        target: 'http://localhost:8080', // 通过代理接口访问实际地址。这里是实际访问的地址。vue会通过代理服务器来代理请求
+        target: 'https://cj-interview-server.onrender.com', // 通过代理接口访问实际地址。这里是实际访问的地址。vue会通过代理服务器来代理请求
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '') // 将api替换为空
       }
