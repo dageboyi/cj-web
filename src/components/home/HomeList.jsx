0c5bd594031fce6e73d178249da475802db9a57f
dageboyi <75166480+dageboyi@users.noreply.github.com>
2023-11-09 10:32:34 +1030

    Update HomeList.jsx

--- src/components/home/HomeList.jsx
+++ src/components/home/HomeList.jsx
@@ -16,11 +16,11 @@ const HomeList = (props) => {
             {
                 props.list.length !== 0 ? props.list.map(item => {
                     return (
-                        <div onClick={() => toDetails(item.articleId)} className={ styles.item } key={item.articleId}>
+                        <div onClick={() => toDetails(item.articleId)} className={styles.item} key={item.articleId}>
                             <div className={styles.userContent}>
                                 <div><img className={styles.image} src={item.avatar} alt="" /></div>
                                 <div className={styles.userRight}>
-                                    <div>{item.username} <span  className={styles.sex} style={{ marginLeft: '10px' }}>Release time: {time(item.time)}</span></div>
+                                    <div>{item.username} <span className={styles.sex} style={{ marginLeft: '10px' }}>Release time: {time(item.time)}</span></div>
                                     <div className={styles.sex}>
                                         <span>Publish survey: {item.count}</span>
                                         <span style={{ marginLeft: '10px' }}>Number of respondents: {item.articleCount}</span>
@@ -28,12 +28,12 @@ const HomeList = (props) => {
                                 </div>
                             </div>
                             <div>
-                                <div style={{ marginTop: '4px' }}>{ item.title }</div>
-                                <div className={styles.content}>{ item.content }</div>
+                                <div style={{ marginTop: '4px' }}>{item.title}</div>
+                                <div className={styles.content}>{item.content}</div>
                             </div>
                         </div>
                     )
-                }) :<div style={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>暂无数据</div>
+                }) : <div style={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>No data yet</div>
             }
         </>
     )
